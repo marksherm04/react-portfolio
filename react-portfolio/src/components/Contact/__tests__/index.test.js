5ea3d7b3691e726565fd8c863b0b964749345725
Mark S <marksherm04@gmail.com>
2021-09-02 15:20:12 -0400

    created tests and passed for Contact section

---
+++ react-portfolio/src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Contact from '..';
+
+afterEach(cleanup);
+
+describe('Contact component', () => {
+	// First Test
+	it('renders', () => {
+		render(<Contact />);
+	});
+	// Second Test
+	it('matches snapshot DOM node structure', () => {
+		// render Contact
+		const { asFragment } = render(<Contact />);
+		expect(asFragment()).toMatchSnapshot();
+	});
+})
+
+

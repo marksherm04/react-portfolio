cd20dc49f29224de0474ce56dd71add1ba529373
Mark S <marksherm04@gmail.com>
2021-09-02 13:26:26 -0400

    adjusted header and made links for About, Contact, and Projects

--- react-portfolio/src/components/Nav/index.js
+++ react-portfolio/src/components/Nav/index.js
@@ -1,32 +1,39 @@
 import React from 'react';
+import { capitalizeFirstLetter } from "../../utils/helpers";
+
 
 function Nav() {
 
+	const categories = [
+		{ name: 'projects', description: 'Photos of different projects of mine' }
+	];
+
+	const handleClick = () => {
+		console.log("click handled");
+	};
+
 	return (
-		<header>
-			<h2>
-				<a href="/">
-					<span role="img" aria-label="camera"> </span> Mark Sherman
-				</a>
-			</h2>
+		<header data-testid="header" className="flex-row px-1">
+			<h1>
+				<span role="img"> Mark Sherman </span>
+			</h1>
 			<nav>
-				<ul className="flex">
+				<ul className="flex-row">
 					<li className="mx-2">
-						<a href="#about">
+						<a href="#about" onClick={() => handleClick()}>
 							About Me
 						</a>
 					</li>
-					<li>
-						<span>Contact</span>
+					<li className={"mx-2"}>
+						<a href="#contact" onClick={() => handleClick()}>
+							Contact
+						</a>
 					</li>
 					{categories.map((category) => (
-						<li
-							className="mx-1"
-							key={category.name}
-							>
-								<span>
-									{category.name}
-								</span>
+						<li className="mx-1" key={category.name} >
+							<a href="#projects" onClick={() => { handleClick(); }}>
+								{capitalizeFirstLetter(category.name)}
+							</a>
 						</li>
 					))}
 				</ul>

1dd982a01eb88426a55fec618caa9fe00b55887d
Mark S <marksherm04@gmail.com>
2021-09-13 17:26:16 -0400

    resume section styled

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,19 +6,19 @@ function Nav(props) {
 	 return (
 		 <nav>
 			 <ul className="text-2xl font-bold text-blue-500 flex py-10 px-4 font-serif cursor-pointer static">
-				 <li className={currentLink === "about" ? "rounded bg-blue-200 hover:bg-yellow-300 navActive" : "mx-6"}>
+				 <li className={currentLink === "about" ? "rounded bg-blue-200 hover:bg-yellow-300 navActive" : "m-8 md:my-8"}>
 					 <span onClick={() => setCurrentLink('about')}>About Me</span>
 				 </li>
-				 <li className={currentLink === "portfolio" ? "rounded bg-blue-200 hover:bg-yellow-300 navActive" : "mx-6"}>
+				 <li className={currentLink === "portfolio" ? "rounded bg-blue-200 hover:bg-yellow-300 navActive" : "m-8 md:my-8"}>
 					 <span onClick={() => setCurrentLink("portfolio")}>Portfolio</span>
 				 </li>
-				 <li className={currentLink === "contact" ? "rounded bg-blue-200 hover:bg-yellow-300 navActive" : "mx-6"}>
+				 <li className={currentLink === "contact" ? "rounded bg-blue-200 hover:bg-yellow-300 navActive" : "m-8 md:my-8"}>
 					 <span onClick={() => setCurrentLink("contact")}>Contact</span>
 				 </li>
-				 <li className={currentLink === "resume" ? "rounded bg-blue-200 hover:bg-yellow-300 navActive" : "mx-8"}>
+				 <li className={currentLink === "resume" ? "rounded bg-blue-200 hover:bg-yellow-300 navActive" : "m-8 md:my-8"}>
 					 <span onClick={() => setCurrentLink("resume")}>Resume</span>
 				 </li>
-				 <li className={currentLink === "name" ? "navActive" : "mx-48"}>
+				 <li className={currentLink === "name" ? "navActive" : "m-8"}>
 					 <span onClick={() => setCurrentLink("about")}>Mark Sherman 👨‍💻</span>
 				 </li>
 			 </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'xsm': {'min': '375px', 'max': '766px'},
+      'sm': {'min': '640px', 'max': '767px'},
+      'md': {'min': '768px', 'max': '1023px'},
+      'lg': {'min': '1024px', 'max': '1279px'},
+      'xl': {'min': '1280px', 'max': '1535px'},
+      '2xl': {'min': '1536px'},
+    },
     extend: {
       backgroundImage: {}
     },

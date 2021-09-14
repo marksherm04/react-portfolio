2a2d0007ad1840ae89514a7454f493faec399dbf
Mark S <marksherm04@gmail.com>
2021-09-14 11:09:27 -0400

    took out duplicate code in server.js

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
-const express = require('express');
-const app = express();
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));

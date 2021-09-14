7271e87f6eb33172c4f897c02884be7fb3dc6049
Mark S <marksherm04@gmail.com>
2021-09-14 10:36:12 -0400

    added social icons that work

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,49 +1,17 @@
-import React, { Components } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { SocialIcon } from 'react-social-icons';
 
 function Footer() {
 	return (
 		<footer className="footer">
-			<div>
-				<a
-					href="https://github.com/marksherm04"
-					target="_blank"
-					rel="noopener norefferer"
-				>
-					<img
-						src={require("../../components/icons/github-icon.jpg")}
-						alt="Github"
-						className="icon"
-					></img>
-				</a>
-			</div>
-			<div>
-				<a
-					href="https://www.linkedin.com/in/mark-sherman-82a072208/"
-					target="_blank"
-					rel="noopener norefferer"
-				>
-					<img
-						src={require("../../components/icons/linkedin-icon.jpg")}
-						alt="Linkedin"
-						className="icon"
-					></img>
-				</a>
-			</div>
+			<div className="flex justify-around bg-gradient-to-r from-yellow-300 via-blue-200 to-yellow-300">
+				<SocialIcon url="https://twitter.com/marksherman6" />
 
-			<div>
-				<a
-					href="https://twitter.com/marksherman6"
-					target="_blank"
-					rel="noopener norefferer"
-				>
-					<img
-						src={require("../../components/icons/twitter-icon.jpg")}
-						alt="Twitter"
-						className="icon"
-					></img>
-				</a>
-			</div>
+				<SocialIcon url="https://github.com/marksherm04" />
 
+				<SocialIcon url="https://www.linkedin.com/in/mark-sherman-82a072208/" />
+			</div>
 		</footer>
 	);
 }
